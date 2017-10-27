7250181da7f179053186348d9ae3a8f1037e5a4f
SvavarHall <svavarhall@siminn.is>
2017-10-27 21:33:25 +0000

    removed postman token from sample

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 var request = require("request");
 
-var options = { method: 'GET',
-  url: 'https://www.vinbudin.is/addons/origo/module/ajaxwebservices/search.asmx/DoSearch',
-  qs: 
-   { category: 'white',
-     skip: '0',
-     count: '999',
-     orderBy: 'random',
-     shop: '105' },
-  headers: 
-   { 'postman-token': '30a97b68-edf4-53f4-c8b5-75de9f783e0e',
-     'cache-control': 'no-cache',
-     'content-type': 'application/json' } };
+var options = {
+    method: 'GET',
+    url: 'https://www.vinbudin.is/addons/origo/module/ajaxwebservices/search.asmx/DoSearch',
+    qs: {
+        category: 'white',
+        skip: '0',
+        count: '999',
+        orderBy: 'random',
+        shop: '105'
+    },
+    headers: {
+        'cache-control': 'no-cache',
+        'content-type': 'application/json'
+    }
+};
 
 request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+    if (error) throw new Error(error);
 
-  console.log(body);
+    console.log(body);
 });
