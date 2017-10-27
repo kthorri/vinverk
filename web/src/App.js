c731a0fae07a5671bbd29ea2cf46749a6750e7ef
Suganthi <suganthikrishnavathi@gmail.com>
2017-10-27 23:44:33 +0100

    Update home page with wine glass

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,17 @@
 // @flow
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import wineGlasses from './Cheers.jpg';
 import './App.css';
 
 class App extends Component<*> {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <img src={wineGlasses} className="wine-glasses" alt="logo" width={"30%"}/>
+                <h1 className="App-title">Cheers!</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
